b5d23c8da0142d0f8bce930cbde5b4b64961d2e8
MascaHello <francesco.mas19@live.it>
2022-02-13 01:19:53 +0100

    created header  + variable scss

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import './Header.scss';
+export function Header() {
+    return (
+        <header>
+            <h1>Game of life</h1>
+        </header>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/common.scss';
 import App from './pages/App';
+import {Header} from './components/Header/Header';
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <Header />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
