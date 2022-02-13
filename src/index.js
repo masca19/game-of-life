34f40be2a494678615da16eded7646d2c1e2ccad
MascaHello <francesco.mas19@live.it>
2022-02-13 01:21:00 +0100

    installed react-drag-drop-files + refactor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/common.scss';
-import App from './pages/App';
+import App from './pages/App/App';
 import {Header} from './components/Header/Header';
 
 ReactDOM.render(
